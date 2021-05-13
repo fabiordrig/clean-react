1f1c5aaa53bb26b15acff10ab7cfcfeff3120af2
Fabio Rodrigues <fabiordrig@gmail.com>
2021-05-13 16:51:50 -0300

    feat: add login and spinner component

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import Styles from './spinner-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+const Spinner: FC<Props> = (props: Props) => {
+  return (
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default Spinner
