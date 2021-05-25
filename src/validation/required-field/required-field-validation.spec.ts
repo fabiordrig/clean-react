789def4c0ff450295c6f7fe1534a297923fcda88
Fabio Rodrigues <fabiordrig@gmail.com>
2021-05-25 18:29:11 -0300

    feat: ensure requiredFieldValidation return empty if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,11 +1,20 @@
+import faker from 'faker'
 import { RequiredFieldError } from '../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.datatype.string())
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
+  })
+  test('Should return false if field is not empty', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.datatype.string())
+
+    expect(error).toBeFalsy()
   })
 })
