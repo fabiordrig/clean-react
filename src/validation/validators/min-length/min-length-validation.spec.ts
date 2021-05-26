16e27f1ee1d4b15a755e54b2b2605c8d14c556cf
Fabio Rodrigues <fabiordrig@gmail.com>
2021-05-26 18:37:28 -0300

    feat: ensure MinLengthValidation returns falsy if value is valid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { MinLengthValidation } from './min-length-validation'
+import faker from 'faker'
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.datatype.string(), 3)
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.datatype.string(1))
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if value is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+})
---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,9 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols'
+
+export class MinLengthValidation implements FieldValidation {
+  constructor (readonly name: string, private readonly minLength: number) {}
+  validate (value: string): Error {
+    return value.length >= this.minLength ? null : new InvalidFieldError()
+  }
+}
